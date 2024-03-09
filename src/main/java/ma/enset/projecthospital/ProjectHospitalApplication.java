202434de15691234092e0a7217d18222f5156095
MOUSSA <moumen.moussa@gmail.com>
2024-03-10 00:12:03 +0100

    message commit

---
+++ src/main/java/ma/enset/projecthospital/ProjectHospitalApplication.java
@@ -0,0 +1,33 @@
+package ma.enset.projecthospital;
+
+import ma.enset.projecthospital.entities.Patient;
+import ma.enset.projecthospital.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class ProjectHospitalApplication  implements CommandLineRunner {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProjectHospitalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        patientRepository.save(new Patient(null,"MOUSSA",new Date(),false,344));
+        patientRepository.save(new Patient(null,"OUMAIMA",new Date(),false,300));
+        patientRepository.save(new Patient(null,"AMIN",new Date(),false,344));
+        patientRepository.save(new Patient(null,"HAJAR",new Date(),false,344));
+        patientRepository.save(new Patient(null,"AYOUB",new Date(),false,344));
+
+    }
+}
---
+++ src/main/java/ma/enset/projecthospital/web/PatientController.java
@@ -0,0 +1,40 @@
+package ma.enset.projecthospital.web;
+
+import lombok.AllArgsConstructor;
+import ma.enset.projecthospital.entities.Patient;
+import ma.enset.projecthospital.repositories.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+
+    private PatientRepository patientRepository;
+
+    @GetMapping("/index")
+    public String index(Model model, @RequestParam(name="p",defaultValue = "0") int p,
+                        @RequestParam(name="s",defaultValue ="4") int s,
+                        @RequestParam(name="keyword",defaultValue ="") String kw
+    ){
+
+        Page<Patient> patientPage=patientRepository.findByNomContains(kw,PageRequest.of(p,s));
+        model.addAttribute("ListPatients",patientPage.getContent());
+        model.addAttribute("pages",new int[patientPage.getTotalPages()]);
+        model.addAttribute("currentPage",p);
+        model.addAttribute("keyword",kw);
+        return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Long id,String keyword,int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+}
