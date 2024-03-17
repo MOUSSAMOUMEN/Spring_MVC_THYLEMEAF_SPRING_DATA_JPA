719bce7495e0e706887cdf47e39bdc2d60f75f14
kival <moumen.moussa@gmail.com>
2024-03-17 01:22:18 +0000

    message commit

---
+++ src/main/java/ma/enset/projecthospital/security/securityConfig.java
@@ -0,0 +1,45 @@
+package ma.enset.projecthospital.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+
+        return new InMemoryUserDetailsManager(
+
+                User.withUsername("user1").password(passwordEncoder.encode("12345")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("12345")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("12345")).roles("USER","ADMIN").build()
+
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .formLogin() // Configuration de la connexion via le formulaire
+                .and()
+                .authorizeRequests() // Configuration des autorisations des requêtes
+                .anyRequest().authenticated(); // Toutes les autres requêtes nécessitent une authentification
+
+        return httpSecurity.build();
+    }
+
+}
