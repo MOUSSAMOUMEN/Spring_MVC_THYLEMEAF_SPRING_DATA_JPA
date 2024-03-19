7e382b453a35ae2a1aa643dfd175221bfd803012
kival <moumen.moussa@gmail.com>
2024-03-19 14:39:10 +0000

    message commit

---
+++ src/main/java/ma/enset/projecthospital/security/SecurityConfig.java
@@ -0,0 +1,2 @@
+package ma.enset.projecthospital.security;public class SecurityConfig {
+}
--- src/main/java/ma/enset/projecthospital/security/securityConfig.java
+++ src/main/java/ma/enset/projecthospital/security/securityConfig.java
@@ -1,9 +1,8 @@
 package ma.enset.projecthospital.security;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,33 +12,26 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class securityConfig {
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder){
+        String encodedPassword = passwordEncoder.encode("1234");
+        System.out.println(encodedPassword);
         return new InMemoryUserDetailsManager(
-
-                User.withUsername("user1").password(passwordEncoder.encode("12345")).roles("USER").build(),
-                User.withUsername("user2").password(passwordEncoder.encode("12345")).roles("USER").build(),
-                User.withUsername("admin").password(passwordEncoder.encode("12345")).roles("USER","ADMIN").build()
-
+                User.withUsername("user1").password(encodedPassword).roles("USER").build(),
+                User.withUsername("user2").password(encodedPassword).roles("USER").build(),
+                User.withUsername("admin").password(encodedPassword).roles("USER","ADMIN").build()
         );
     }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity
-                .formLogin() // Configuration de la connexion via le formulaire
-                .and()
-                .authorizeRequests() // Configuration des autorisations des requêtes
-                .anyRequest().authenticated(); // Toutes les autres requêtes nécessitent une authentification
-
-        return httpSecurity.build();
+        return httpSecurity
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/deletePatient/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .build();
     }
-
 }
